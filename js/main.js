19517e4ecc05a3a905a98fe8c892bcc18b162acc
Gregory Croisdale <gcroisda@vols.utk.edu>
2020-08-29 11:08:43 -0400

    reformatting

--- js/main.js
+++ js/main.js
@@ -6,49 +6,50 @@ let data      = -1;
 let options   = -1;
 let network   = -1;
 let output    = -1;
+let input     = -1;
+let states    = -1;
 
 // perform necessary tasks upon page load
 function init() {
-  // assign important variables
+  // initialize important variables
   output = document.getElementById("output");
+  output = document.getElementById("input");
+
+  // special formats for network vis
+  options = {};
+  edges = new vis.DataSet([]);
+  nodes = new vis.DataSet([]);
+
+  // create the network
+  container = document.getElementById("graph");
+  network = new vis.Network(container, data, options);
 
   // set GUI to default state
   output.value = "";
 
   // Test state
-  test = [new State(0, "Hewwo", [], false)];
+  states = [new State(0, "Hewwo", [], false)];
   // create an array with nodes
-  nodes = new vis.DataSet([
+  /*nodes = new vis.DataSet([
       test[0]
   ]);
 
   // create an array with edges
   edges = new vis.DataSet([
     test[0].transitions["e"],
-//      {id: 0, arrows: "to", label: "test", from: 1, to: 3},
-//      {from: 1, to: 2},
-//      {from: 2, to: 4},
-//      {from: 2, to: 5}
-  ]);
-
-  // create a network
-  container = document.getElementById("graph");
-
+  //      {id: 0, arrows: "to", label: "test", from: 1, to: 3},
+  //      {from: 1, to: 2},
+  //      {from: 2, to: 4},
+  //      {from: 2, to: 5}
+  ]);*/
   console.log(container);
-
-  // provide the data in the vis format
-  data = {
-      nodes: nodes,
-      edges: edges
-  };
-  options = {};
-
-  // initialize your network!
-  network = new vis.Network(container, data, options);
 }
 
 function update() {
   data.nodes.update([{id:0, label:"hi"}]);
-  console.log(test);
   output.value = JSON.stringify(test) + ":)";
+}
+
+function parse() {
+  output.value = JSON.parse(input.value);
 }
