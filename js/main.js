f4decfc27045469ea24cb21d8dc23ae591fa8e80
Gregory Croisdale <gcroisda@vols.utk.edu>
2020-08-29 16:44:08 -0400

    THIS IS NOT A PRANK -- you can ADD EDGES now

--- js/main.js
+++ js/main.js
@@ -10,17 +10,38 @@ let states    = -1;
 let network   = -1;
 const verbose =  0;
 let menu      = -1;
-let node_menu = false;
+let node_name = "";
 
-const n_menu_html =  `
+
+const new_node_menu_html =  `
 <div id="m_head_container"></div>
 <button onclick=\"close_menu()\" id=\"close\">✖</button>
 <div class="m_head">New node menu</div><br>
 New label:
-<input type="text" id="new_label" placeholder="label"><br><br>
+<input type="text" id="new_label" placeholder="Label"><br><br>
 Final state? <input type="checkbox" id="final"><br><br>
 <button onclick="node_from_menu()" style="width: 100%;">Create node</button>
-`
+`;
+
+const node_menu_html_top =  `
+<div id="m_head_container"></div>
+<button onclick=\"close_menu()\" id=\"close\">✖</button>
+<div class="m_head">Edit node menu</div><br><center style="margin-top: -10px;">Node: `;
+
+const node_menu_html_bottom =  `
+</center>
+Add transition:
+<input type="text" id="to" placeholder="To" style="width: 49%;"><input type="text" id="symbol" placeholder="Symbol" style="width: 49%;"><br>
+<button onclick="add_edge()" style="width: 100%;">Add edge</button>
+
+Change label:
+<input type="text" id="new_label" placeholder="Label"><br>
+<button onclick="node_from_menu()" style="width: 100%;">Change label</button>
+
+<button onclick="node_from_menu()" style="width: 100%;">Toggle final</button>
+
+<button onclick="node_from_menu()" style="width: 100%;">Delete node</button>
+`;
 
 // perform necessary tasks upon page load
 function init() {
@@ -42,35 +63,53 @@ function init() {
   output.value = "";
 
   // States
-  states = [];
+  states = {};
 
   // Add initial state
-  new_node("Init", "Init");
+  new_node("Init", false);
 
   // listen for event
   document.getElementById("graph").addEventListener("click", on_click);
+  document.getElementById("edit_mode").addEventListener("click", close_menu);
 }
 
+// TODO: Will parse input
 function parse() {
   output.value = JSON.parse(input.value);
 }
 
+// When you click on the graph window,
 function on_click(context) {
   let id, e_id;
+
+  // check if you're on an object
   if ((id = network.getNodeAt(context)) == undefined) {
+    // if not on Node, check edge
     if ((e_id = network.getEdgeAt(context)) == undefined) {
+      // now, let's try to make a new node
+      close_menu();
       new_node_menu(context.clientX, context.clientY);
+    }
+    else {
+      close_menu();
+      alert("Edge editor");
     }
   }
   else {
+    // open the edit menu
     close_menu();
+    node_name = id;
+    node_edit_menu(context.clientX, context.clientY);
   }
 }
 
+// set display to show new node menu
 function new_node_menu(x, y) {
   if (document.getElementById("edit_mode").checked) {
-    node_menu = true;
-    menu.innerHTML = n_menu_html;
+    clicked_x = x;
+    clicked_y = y;
+
+    menu.innerHTML = new_node_menu_html;
     if (verbose) menu.innerHTML += "x: " + x + " y: " + y;
     menu.style["margin-left"] = x;
     menu.style["margin-top"]  = y;
@@ -79,20 +118,37 @@ function new_node_menu(x, y) {
   }
 }
 
+// set display to show node edit menu
+function node_edit_menu(x, y) {
+  if (document.getElementById("edit_mode").checked) {
+    clicked_x = x;
+    clicked_y = y;
+
+    menu.innerHTML = node_menu_html_top + node_name + node_menu_html_bottom;
+    if (verbose) menu.innerHTML += "x: " + x + " y: " + y;
+    menu.style["margin-left"] = x;
+    menu.style["margin-top"]  = y;
+    menu.style["display"]     = "block";
+    if (verbose) console.log("New node menu @ " + x + ", " + y);
+  }
+}
+
+// generate a node from a menu
 function node_from_menu() {
   let new_label = document.getElementById("new_label").value;
 
-  let node_id = nodes.get(new_label);
+  let node_id = nodes.get(new_label.replace(/\s+/g, ''));
   if (node_id != null) {
     document.getElementById("new_label").placeholder = new_label + " already exists.";
     document.getElementById("new_label").value = "";
     network.moveTo(node_id);
   }
   else {
-    new_node(new_label, new_label);
+    new_node(new_label, document.getElementById("final").checked);
   }
 }
 
+// close the menu
 function close_menu(){
   menu.style["display"] = "none";
 }
--- js/states.js
+++ js/states.js
@@ -14,13 +14,20 @@ function Transition(id, symbol, from, to) {
   this.color = default_color;
 }
 
-function State(id, label, transitions, accepted) {
-  this.id = id;
+function State(label, transitions, accepted) {
+  this.id = label.replace(/\s+/g, '');
   this.label = label;
   this.transitions = {
-    "ε": new Transition(n_edges++, "e", this.id, this.id),
+    "ε": [new Transition(n_edges++, "e", this.id, this.id)],
   };
   this.accepted = accepted;
+
+  if (accepted) {
+    this.color = "red";
+  }
+  else {
+    //this.color = "default_color";
+  }
 }
 
 function e_closure(states, nodes) {
@@ -35,21 +42,41 @@ function state_str(s) {
   let out = "";
 }
 
-function new_node(id = input.value, label = input.value, n = network, S = states, E = edges, N = nodes) {
-  s = new State(id, label, [], false)
-  S.push(s);
-  N.update([S[S.length - 1]]);
+function new_node(id = input.value, final = 0, n = network, S = states, E = edges, N = nodes) {
+  s = new State(id, [], final);
+  S[id.replace(/\s+/g, '')] = s;
+  N.update([S[id.replace(/\s+/g, '')]]);
 
   for (const i in s.transitions) {
     if (verbose) console.log(i);
-    E.update([s.transitions[i]]);
+    for (const j in s.transitions[i]) {
+      if (verbose) console.log(i);
+      E.update([s.transitions[i][j]]);
+    }
   }
-
-  id = "";
-
-  n.redraw();
 }
 
 function touch_state(s) {
   let out = "";
+}
+
+function add_edge(n = network, S = states, E = edges, N = nodes) {
+  let to = document.getElementById("to").value.replace(/\s+/g, '');
+  let to_item;
+  let symbol = document.getElementById("symbol").value;
+  let from = node_name.replace(/\s+/g, '');
+  let from_item = N.get(from);
+
+  if (S[to] == undefined) {
+    alert("State " + to + " DNE");
+  }
+  else {
+    to_item = N.get(to);
+    if (S[from].transitions[symbol] == undefined) S[from].transitions[symbol] = []
+    S[from].transitions[symbol].push(new Transition(n_edges++, symbol, from, to))
+    E.update(S[from].transitions[symbol]);
+    console.log(S[from].transitions[symbol]);
+  }
+
+  return 0;
 }
